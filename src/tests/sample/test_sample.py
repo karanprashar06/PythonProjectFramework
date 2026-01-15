60b1a0e7810e2e88eb0710bb917322ae855fb9bf
karan <karanprashar06@gmail.com>
2026-01-15 16:32:51 +0530

    sample test case added

---
+++ src/tests/sample/test_sample.py
@@ -0,0 +1,12 @@
+import pytest
+import allure
+
+@allure.step("Step 1")
+@allure.title("Verify 🛑 Failed !that the Framework is  not working")
+def test_sample_fail():
+    assert True == False
+
+@allure.step("Step 2")
+@allure.title("Verify ✅ Passed that the Framework is working")
+def test_sample_pass():
+    assert True == True
